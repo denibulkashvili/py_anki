a5e57d190ca1ce722153a5473834fd73ae08c875
Denis Bulkashvili <denibulkashvili@gmail.com>
2020-06-09 21:24:55 +0700

    Refactoring anki utils

--- utils/anki_utils.py
+++ utils/anki_utils.py
@@ -1,39 +1,35 @@
 """Anki Generator module"""
 import genanki
 
-SETTINGS = {
-    "deck_id": 2016547257,
-    "deck_name": "My Deck",
-    "model_id": 1283770623,
-    "model_name": "My Model",
+DEFAULT = {
+    "id": 2016547257,
+    "name": "My Deck"
 }
 
+MODEL = genanki.Model(
+    1283770623,
+    "My Model",
+    fields=[{"name": "Question"}, {"name": "Answer"}],
+    templates=[
+        {
+            "name": "Card 1",
+            "qfmt": "{{Question}}",
+            "afmt": '{{Question}}<hr id="answer">{{Answer}}',
+        }
+    ],
+)
+
+def _generate_deck_id(deck_name):
+    """Generates deck id from the deck name"""
+    return deck_name.lower().replace(" ", "_")
 
 class AnkiGenerator:
     """Generates Anki Decks"""
-    def __init__(
-        self, deck_id=2016547257, deck_name="My Deck", model_id=1283770623, model_name="My Model"
-    ):
+    def __init__(self, deck_id=DEFAULT["id"], deck_name=DEFAULT["name"]):
         self.deck_id = deck_id
         self.deck_name = deck_name
-        self.model_id = model_id
-        self.model_name = model_name
-        self.model = self.__create_model()
-
-    def __create_model(self):
-        return genanki.Model(
-            self.model_id,
-            self.model_name,
-            fields=[{"name": "Question"}, {"name": "Answer"}],
-            templates=[
-                {
-                    "name": "Card 1",
-                    "qfmt": "{{Question}}",
-                    "afmt": '{{Question}}<hr id="answer">{{Answer}}',
-                }
-            ],
-        )
-
+        self.model = MODEL
+   
     def create_deck(self):
         """Creates new deck"""
         return genanki.Deck(self.deck_id, self.deck_name)
@@ -47,9 +43,9 @@ class AnkiGenerator:
             fields = note[1:]
             return genanki.Note(model=self.model, guid=note_id, fields=fields)
         except ValueError:
-            print("ValueError")
-            print(note[0])
+            print("ValueError", note[0])
 
-    def create_deck_file(self, deck, file_name="deck.apkg"):
+    @classmethod
+    def create_deck_file(cls, deck, file_name="deck.apkg"):
         """Creates an .apkg deck file"""
         return genanki.Package(deck).write_to_file(file_name)
